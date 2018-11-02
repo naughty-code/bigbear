d57d52401538c97d957a50937539d5757af3775f
Manuel <manuel.c1992@gmail.com>
2018-11-02 14:01:00 -0500

    Add initial support for additional search fields

--- static/assets/report.js
+++ static/assets/report.js
@@ -1,6 +1,35 @@
+
+$.fn.dataTable.ext.search.push(
+    function( settings, data, dataIndex ) {
+		var idvrm_filter = $('#idvrm').val();
+		var id_filter = $('#id_search').val();
+		// var name_filter = $('#name').val();
+		// var checkin_filter = $('#checkin').val();
+		// var checkout_filter = $('#checkout').val();
+		// var status_filter = $('#status').val();
+		// var days_filter = $('#days').val();
+
+        var idvrm_table = data[0];
+		var id_table = data[1];
+		// var name_table = data[2];
+		// var checkin_table = data[4];
+		// var checkout_table = data[5];
+		// var status_table = data[6];
+		// var days_table = data[8];
+ 
+		if ( (id_table != '' && idvrm_table != '') &
+			id_table.search(id_filter) != -1
+			&& idvrm_table.search(idvrm_filter) != -1)
+			return true;
+
+        return false;
+    }
+);
 $(document).ready(function() {
 	var alertErrorReport = $('#alert-danger-report')
 	var progressBarReport = $('#progress-report')
+	var search_table = $('#search-table')
+	var table1 = $('#table1')
 	progressBarReport.removeClass('d-none')
 	alertErrorReport.addClass('d-none')
 
@@ -8,6 +37,8 @@ $(document).ready(function() {
 		$.get('/api/report')
 			.then(function (data) {
 				progressBarReport.addClass('d-none')
+				search_table.removeClass('d-none')
+				table1.removeClass('d-none')
 				var tbody = $('#table1 tbody')
 				data.table1.forEach(data_row => {
 					var row = $('<tr></tr>');
@@ -17,7 +48,7 @@ $(document).ready(function() {
 					$('<td style="text-align: right"></td>').text(data_row[3]).appendTo(row);
 					row.appendTo(tbody);
 				});
-				$('#report').DataTable({
+				var table2 = $('#report').DataTable({
 					data: data.table2,
 					columns: [
 						{ title: "IDVRM" },
@@ -41,6 +72,9 @@ $(document).ready(function() {
 						{ title: "NAME" }
 					]
 				});
+				$('#idvrm, #id_search, #name, #checkin, #checkout, #status, days').keyup( function() {
+					table2.draw();
+				} );
 			})
 			.fail(function() {
 				progressBarReport.addClass('d-none')
