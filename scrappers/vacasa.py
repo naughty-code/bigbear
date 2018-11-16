60a8ffc4a840e72e5f83452a2693439a987b40ae
Manuel <manuel.c1992@gmail.com>
2018-11-16 11:37:53 -0500

    Fix table name in query

--- scrappers/vacasa.py
+++ scrappers/vacasa.py
@@ -457,7 +457,7 @@ def update_last_scrape():
     with connection, connection.cursor() as c:
         c.execute("""
             INSERT INTO db.vrm (idvrm, name, website, ncabins, last_scrape)
-            VALUES (%s, %s, %s, (select count(id) from db.cabins where idvrm = 'VACASA'), now())
+            VALUES (%s, %s, %s, (select count(id) from db.cabin where idvrm = 'VACASA'), now())
             ON CONFLICT (idvrm) DO UPDATE SET name = excluded.name, website = excluded.website, ncabins = excluded.ncabins, last_scrape = excluded.last_scrape
         """, ('VACASA', 'Vacasa', 'https://www.vacasa.com/'))
     connection.close()    
