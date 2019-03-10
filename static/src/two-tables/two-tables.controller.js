c04d56995ab3ceef74e285fd9c3536ee3554034d
Manuel <manuel.c1992@gmail.com>
2019-03-10 18:41:21 -0500

    Fix access to endpoints

--- static/src/two-tables/two-tables.controller.js
+++ static/src/two-tables/two-tables.controller.js
@@ -15,8 +15,8 @@ function twoTablesController($scope, $http, $routeParams) {
 		enableFiltering: true,
 		minimumColumnSize: 150
 	};
-	// $http.get('http://74.91.126.179:5001/api/' + view)
-	$http.get('http://localhost:5001/api/' + view)
+	$http.get('http://74.91.126.179:5001/api/' + view)
+	// $http.get('http://localhost:5001/api/' + view)
 		.then(function(response) {
 			for (const key in response.data.table1[0]) {
 				if (response.data.table1[0].hasOwnProperty(key)) {
